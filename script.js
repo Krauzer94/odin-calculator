851ad1b66b5f440f11fc31f0593f01603ea5474d
KrauzerPH94 <13894059+KrauzerPH94@users.noreply.github.com>
2023-11-08 00:28:33 -0300

    Main mathematical operations validated Used logs and simple return functions

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 // Functions for each math operation
 // Add, subctract, multiply and divide
 // Test tem directly into the console
+
+// Add operation
+function add(a, b) {
+    return a + b;
+}
+
+// Subtract operation
+function sub(a, b) {
+    return a - b;
+}
+
+// Multiply operation
+function mul(a, b) {
+    return a * b;
+}
+
+// Divide operation
+function dvd(a, b) {
+    return a / b;
+}
+
+// Test numbers
+let a = 1;
+let b = 2;
+
+// Log validation
+console.log(`Numbers: 1 and 2`)
+console.log(`Adding: ${add(a, b)}`);
+console.log(`Subtracting: ${sub(a, b)}`);
+console.log(`Multiplying: ${mul(a, b)}`);
+console.log(`DIviding: ${dvd(a, b)}`);
